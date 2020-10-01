9388cba55d3700b459f9e36bc01ab8af560fb81c
Scott <scotteau@gmail.com>
2020-10-02 06:49:39 +0800

    added input field to edit post title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {ListComponent} from './components/list/list.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ListItemComponent } from './components/list-item/list-item.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,10 +13,11 @@ import { ListItemComponent } from './components/list-item/list-item.component';
     ListComponent,
     ListItemComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter, ViewChild, ElementRef} from '@angular/core';
 import {Item} from '../../models/interfaces';
 
 @Component({
@@ -8,6 +8,7 @@ import {Item} from '../../models/interfaces';
   template: `
     <div>
       <p>{{item.post.title}}</p>
+      <input [(ngModel)]="item.post.title" type="text" (keyup.enter)="blur()" #titleInput>
       <p>{{item.album.title}}</p>
       <p>{{item.user.name}}</p>
       <button (click)="delete()">delete item</button>
@@ -18,6 +19,7 @@ export class ListItemComponent implements OnInit {
 
   @Input() item: Item;
   @Output() onDeleteItem = new EventEmitter<number>();
+  @ViewChild('titleInput') titleInput: ElementRef;
 
   constructor() {
   }
@@ -27,5 +29,9 @@ export class ListItemComponent implements OnInit {
 
   delete(): void {
     this.onDeleteItem.emit(this.item.post.id);
+  }
+
+  blur() {
+    this.titleInput.nativeElement.blur();
   }
 }
